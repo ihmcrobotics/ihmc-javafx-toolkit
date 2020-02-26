7c6bd833543b7c8fedf91274af332cb7cf3460d6
Sylvain Bertrand <sbertrand@ihmc.us>
2020-02-26 17:23:52 -0600

    Bump version number.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.14.0"
+   version = "0.14.1"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-javafx-toolkit"
    openSource = true
    
