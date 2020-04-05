9e0784a0257693863b8e10033a9ea4e147eecec3
Sylvain Bertrand <sbertrand@ihmc.us>
2020-04-04 20:47:30 -0500

    :arrow_up: euclid 0.13.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ mainDependencies {
       exclude(group = "org.slf4j", module = "slf4j-simple")
    }
 
-   api("us.ihmc:euclid:0.12.2")
+   api("us.ihmc:euclid:0.13.0")
    api("us.ihmc:ihmc-yovariables:0.4.0")
    api("us.ihmc:ihmc-messager:0.1.4")
    api("us.ihmc:ihmc-graphics-description:0.14.1")
