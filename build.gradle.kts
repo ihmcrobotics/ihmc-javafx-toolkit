a7c947423d78cb52467844b8a312c9f160b1e2ce
Sylvain Bertrand <sbertrand@ihmc.us>
2020-04-06 11:24:00 -0500

    :arrow_up: ihmc-graphics-description 0.15.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ mainDependencies {
    api("us.ihmc:euclid:0.13.0")
    api("us.ihmc:ihmc-yovariables:0.5.0")
    api("us.ihmc:ihmc-messager:0.1.4")
-   api("us.ihmc:ihmc-graphics-description:0.14.1")
+   api("us.ihmc:ihmc-graphics-description:0.15.0")
    api("us.ihmc:jassimp:4.0.0-ihmc5")
 
    var javaFXVersion = "11.0.2"
