97e5facbac0b64429c19d63df12ecee5077698a4
Sylvain Bertrand <sbertrand@ihmc.us>
2020-08-15 19:00:55 -0500

    :arrow_up: ihmc-yovariables 0.9.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,9 +32,9 @@ mainDependencies {
    }
 
    api("us.ihmc:euclid:0.15.0")
-   api("us.ihmc:ihmc-yovariables:0.9.0-alpha-1")
+   api("us.ihmc:ihmc-yovariables:0.9.0")
    api("us.ihmc:ihmc-messager:0.1.5")
-   api("us.ihmc:ihmc-graphics-description:0.18.0-yovariables-0.9.0-alpha-1")
+   api("us.ihmc:ihmc-graphics-description:0.19.0")
    api("us.ihmc:jassimp:4.0.0-ihmc5")
 
    var javaFXVersion = "11.0.2"
