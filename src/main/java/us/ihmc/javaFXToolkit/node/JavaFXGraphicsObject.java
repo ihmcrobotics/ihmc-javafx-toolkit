ca140fffc8ae196f79f728931526a37f06468386
Sylvain Bertrand <sbertrand@ihmc.us>
2020-08-30 21:19:30 -0500

    Switched to the new GeometryDescription.

--- src/main/java/us/ihmc/javaFXToolkit/node/JavaFXGraphicsObject.java
+++ src/main/java/us/ihmc/javaFXToolkit/node/JavaFXGraphicsObject.java
@@ -19,13 +19,7 @@ import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.graphicsDescription.Graphics3DObject;
 import us.ihmc.graphicsDescription.appearance.AppearanceDefinition;
 import us.ihmc.graphicsDescription.appearance.YoAppearanceRGBColor;
-import us.ihmc.graphicsDescription.instructions.ArcTorusGraphics3DInstruction;
-import us.ihmc.graphicsDescription.instructions.CapsuleGraphics3DInstruction;
-import us.ihmc.graphicsDescription.instructions.ConeGraphics3DInstruction;
-import us.ihmc.graphicsDescription.instructions.CubeGraphics3DInstruction;
-import us.ihmc.graphicsDescription.instructions.CylinderGraphics3DInstruction;
-import us.ihmc.graphicsDescription.instructions.EllipsoidGraphics3DInstruction;
-import us.ihmc.graphicsDescription.instructions.ExtrudedPolygonGraphics3DInstruction;
+import us.ihmc.graphicsDescription.instructions.GeometryGraphics3DInstruction;
 import us.ihmc.graphicsDescription.instructions.Graphics3DAddExtrusionInstruction;
 import us.ihmc.graphicsDescription.instructions.Graphics3DAddHeightMapInstruction;
 import us.ihmc.graphicsDescription.instructions.Graphics3DAddMeshDataInstruction;
@@ -33,13 +27,6 @@ import us.ihmc.graphicsDescription.instructions.Graphics3DAddModelFileInstructio
 import us.ihmc.graphicsDescription.instructions.Graphics3DInstruction;
 import us.ihmc.graphicsDescription.instructions.Graphics3DInstructionExecutor;
 import us.ihmc.graphicsDescription.instructions.Graphics3DPrimitiveInstruction;
-import us.ihmc.graphicsDescription.instructions.HemiEllipsoidGraphics3DInstruction;
-import us.ihmc.graphicsDescription.instructions.PolygonGraphics3DInstruction;
-import us.ihmc.graphicsDescription.instructions.PrimitiveGraphics3DInstruction;
-import us.ihmc.graphicsDescription.instructions.PyramidCubeGraphics3DInstruction;
-import us.ihmc.graphicsDescription.instructions.SphereGraphics3DInstruction;
-import us.ihmc.graphicsDescription.instructions.TruncatedConeGraphics3DInstruction;
-import us.ihmc.graphicsDescription.instructions.WedgeGraphics3DInstruction;
 import us.ihmc.graphicsDescription.instructions.primitives.Graphics3DRotateInstruction;
 import us.ihmc.graphicsDescription.instructions.primitives.Graphics3DScaleInstruction;
 import us.ihmc.graphicsDescription.instructions.primitives.Graphics3DTranslateInstruction;
@@ -205,154 +192,12 @@ public class JavaFXGraphicsObject extends Graphics3DInstructionExecutor
    }
 
    @Override
-   protected void doAddPrimitiveInstruction(PrimitiveGraphics3DInstruction primitiveInstruction)
+   protected void doAddGeometryInstruction(GeometryGraphics3DInstruction instruction)
    {
-      if (primitiveInstruction instanceof CubeGraphics3DInstruction)
-      {
-         CubeGraphics3DInstruction cubeInstruction = (CubeGraphics3DInstruction) primitiveInstruction;
-
-         MeshDataHolder meshData = MeshDataGenerator.Box(cubeInstruction.getLength(),
-                                                          cubeInstruction.getWidth(),
-                                                          cubeInstruction.getHeight(),
-                                                          cubeInstruction.getCenteredInTheCenter());
-         Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData, cubeInstruction.getAppearance());
-
-         doAddMeshDataInstruction(meshDataInstruction);
-      }
-      else if (primitiveInstruction instanceof SphereGraphics3DInstruction)
-      {
-         SphereGraphics3DInstruction sphereInstruction = (SphereGraphics3DInstruction) primitiveInstruction;
-
-         MeshDataHolder meshData = MeshDataGenerator.Sphere(sphereInstruction.getRadius(),
-                                                            sphereInstruction.getResolution(),
-                                                            sphereInstruction.getResolution());
-         Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData, sphereInstruction.getAppearance());
-
-         doAddMeshDataInstruction(meshDataInstruction);
-      }
-      else if (primitiveInstruction instanceof WedgeGraphics3DInstruction)
-      {
-         WedgeGraphics3DInstruction wedgeInstruction = (WedgeGraphics3DInstruction) primitiveInstruction;
-
-         MeshDataHolder meshData = MeshDataGenerator.Wedge(wedgeInstruction.getLengthX(), wedgeInstruction.getWidthY(), wedgeInstruction.getHeightZ());
-         Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData, wedgeInstruction.getAppearance());
-
-         doAddMeshDataInstruction(meshDataInstruction);
-      }
-      else if (primitiveInstruction instanceof CapsuleGraphics3DInstruction)
-      {
-         CapsuleGraphics3DInstruction capsuleInstruction = (CapsuleGraphics3DInstruction) primitiveInstruction;
-
-         MeshDataHolder meshData = MeshDataGenerator.Capsule(capsuleInstruction.getHeight(),
-                                                             capsuleInstruction.getXRadius(),
-                                                             capsuleInstruction.getYRadius(),
-                                                             capsuleInstruction.getZRadius(),
-                                                             capsuleInstruction.getResolution(),
-                                                             capsuleInstruction.getResolution());
-         Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData, capsuleInstruction.getAppearance());
-
-         doAddMeshDataInstruction(meshDataInstruction);
-      }
-      else if (primitiveInstruction instanceof EllipsoidGraphics3DInstruction)
-      {
-         EllipsoidGraphics3DInstruction ellipsoidInstruction = (EllipsoidGraphics3DInstruction) primitiveInstruction;
-
-         MeshDataHolder meshData = MeshDataGenerator.Ellipsoid(ellipsoidInstruction.getXRadius(),
-                                                               ellipsoidInstruction.getYRadius(),
-                                                               ellipsoidInstruction.getZRadius(),
-                                                               ellipsoidInstruction.getResolution(),
-                                                               ellipsoidInstruction.getResolution());
-         Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData, ellipsoidInstruction.getAppearance());
-         doAddMeshDataInstruction(meshDataInstruction);
-      }
-      else if (primitiveInstruction instanceof CylinderGraphics3DInstruction)
-      {
-         CylinderGraphics3DInstruction cylinderInstruction = (CylinderGraphics3DInstruction) primitiveInstruction;
-
-         MeshDataHolder meshData = MeshDataGenerator.Cylinder(cylinderInstruction.getRadius(),
-                                                              cylinderInstruction.getHeight(),
-                                                              cylinderInstruction.getResolution(),
-                                                              false);
-         Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData, cylinderInstruction.getAppearance());
-         doAddMeshDataInstruction(meshDataInstruction);
-      }
-      else if (primitiveInstruction instanceof ConeGraphics3DInstruction)
-      {
-         ConeGraphics3DInstruction coneInstruction = (ConeGraphics3DInstruction) primitiveInstruction;
-
-         MeshDataHolder meshData = MeshDataGenerator.Cone(coneInstruction.getHeight(), coneInstruction.getRadius(), coneInstruction.getResolution());
-         Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData, coneInstruction.getAppearance());
-         doAddMeshDataInstruction(meshDataInstruction);
-      }
-      else if (primitiveInstruction instanceof TruncatedConeGraphics3DInstruction)
-      {
-         TruncatedConeGraphics3DInstruction truncatedConeInstruction = (TruncatedConeGraphics3DInstruction) primitiveInstruction;
-
-         MeshDataHolder meshData = MeshDataGenerator.TruncatedCone(truncatedConeInstruction.getHeight(),
-                                                                      truncatedConeInstruction.getXBaseRadius(),
-                                                                      truncatedConeInstruction.getYBaseRadius(),
-                                                                      truncatedConeInstruction.getXTopRadius(),
-                                                                      truncatedConeInstruction.getYTopRadius(),
-                                                                      truncatedConeInstruction.getResolution());
-         Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData, truncatedConeInstruction.getAppearance());
-         doAddMeshDataInstruction(meshDataInstruction);
-      }
-      else if (primitiveInstruction instanceof HemiEllipsoidGraphics3DInstruction)
-      {
-         HemiEllipsoidGraphics3DInstruction hemiEllipsoidInstruction = (HemiEllipsoidGraphics3DInstruction) primitiveInstruction;
-
-         MeshDataHolder meshData = MeshDataGenerator.HemiEllipsoid(hemiEllipsoidInstruction.getXRadius(),
-                                                                   hemiEllipsoidInstruction.getYRadius(),
-                                                                   hemiEllipsoidInstruction.getZRadius(),
-                                                                   hemiEllipsoidInstruction.getResolution(),
-                                                                   hemiEllipsoidInstruction.getResolution());
-         Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData, hemiEllipsoidInstruction.getAppearance());
-         doAddMeshDataInstruction(meshDataInstruction);
-      }
-      else if (primitiveInstruction instanceof ArcTorusGraphics3DInstruction)
-      {
-         ArcTorusGraphics3DInstruction arcTorusInstruction = (ArcTorusGraphics3DInstruction) primitiveInstruction;
-
-         MeshDataHolder meshData = MeshDataGenerator.ArcTorus(arcTorusInstruction.getStartAngle(),
-                                                              arcTorusInstruction.getEndAngle(),
-                                                              arcTorusInstruction.getMajorRadius(),
-                                                              arcTorusInstruction.getMinorRadius(),
-                                                              arcTorusInstruction.getResolution());
-         Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData, arcTorusInstruction.getAppearance());
-         doAddMeshDataInstruction(meshDataInstruction);
-      }
-      else if (primitiveInstruction instanceof PyramidCubeGraphics3DInstruction)
-      {
-         PyramidCubeGraphics3DInstruction pyramidInstruction = (PyramidCubeGraphics3DInstruction) primitiveInstruction;
-
-         MeshDataHolder meshData = MeshDataGenerator.PyramidBox(pyramidInstruction.getLengthX(),
-                                                                 pyramidInstruction.getWidthY(),
-                                                                 pyramidInstruction.getHeightZ(),
-                                                                 pyramidInstruction.getPyramidHeight());
-         Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData, pyramidInstruction.getAppearance());
-         doAddMeshDataInstruction(meshDataInstruction);
-      }
-      else if (primitiveInstruction instanceof PolygonGraphics3DInstruction)
-      {
-         PolygonGraphics3DInstruction polygonInstruction = (PolygonGraphics3DInstruction) primitiveInstruction;
-
-         MeshDataHolder meshData = MeshDataGenerator.PolygonCounterClockwise(polygonInstruction.getPolygonPoints());
-         Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData, polygonInstruction.getAppearance());
-         doAddMeshDataInstruction(meshDataInstruction);
-      }
-      else if (primitiveInstruction instanceof ExtrudedPolygonGraphics3DInstruction)
-      {
-         ExtrudedPolygonGraphics3DInstruction extrudedPolygonInstruction = (ExtrudedPolygonGraphics3DInstruction) primitiveInstruction;
-
-         MeshDataHolder meshData = MeshDataGenerator.ExtrudedPolygon(extrudedPolygonInstruction.getPolygonPoints(),
-                                                                     extrudedPolygonInstruction.getExtrusionHeight());
-         Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData,
-                                                                                                           extrudedPolygonInstruction.getAppearance());
-         doAddMeshDataInstruction(meshDataInstruction);
-      }
-      else
-      {
-         throw new RuntimeException("Need to support that primitive type! primitiveInstruction = " + primitiveInstruction);
-      }
+      MeshDataHolder meshData = MeshDataGenerator.Mesh(instruction.getGeometry());
+      if (meshData == null)
+         throw new RuntimeException("Need to support that primitive type! primitiveInstruction = " + instruction);
+      Graphics3DAddMeshDataInstruction meshDataInstruction = Graphics3DObject.createMeshDataInstruction(meshData, instruction.getAppearance());
+      doAddMeshDataInstruction(meshDataInstruction);
    }
 }
