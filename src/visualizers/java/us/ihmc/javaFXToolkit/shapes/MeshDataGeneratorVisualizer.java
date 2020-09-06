a0c21a655ceeb6603b757c54de865dfb56c8d212
Sylvain Bertrand <sbertrand@ihmc.us>
2020-09-05 20:09:18 -0500

    Added shapes and 3D labels for the axes.

--- src/visualizers/java/us/ihmc/javaFXToolkit/shapes/MeshDataGeneratorVisualizer.java
+++ src/visualizers/java/us/ihmc/javaFXToolkit/shapes/MeshDataGeneratorVisualizer.java
@@ -1,36 +1,51 @@
 package us.ihmc.javaFXToolkit.shapes;
 
+import static us.ihmc.javaFXToolkit.graphics.JavaFXMeshDataInterpreter.interpretMeshData;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+import org.fxyz3d.shapes.primitives.Text3DMesh;
+import org.fxyz3d.shapes.primitives.TexturedMesh;
+
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.MeshView;
 import javafx.stage.Stage;
+import us.ihmc.euclid.axisAngle.AxisAngle;
 import us.ihmc.euclid.geometry.ConvexPolygon2D;
 import us.ihmc.euclid.geometry.tools.EuclidGeometryRandomTools;
+import us.ihmc.euclid.orientation.interfaces.Orientation3DReadOnly;
+import us.ihmc.euclid.shape.convexPolytope.tools.EuclidPolytopeFactories;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
 import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.graphicsDescription.mesh.MeshDataGenerator;
-import us.ihmc.javaFXToolkit.graphics.JavaFXMeshDataInterpreter;
+import us.ihmc.javaFXToolkit.JavaFXTools;
+import us.ihmc.javaFXToolkit.cameraControllers.FocusBasedCameraMouseEventHandler;
 import us.ihmc.javaFXToolkit.scenes.View3DFactory;
 import us.ihmc.javaFXToolkit.starter.ApplicationRunner;
+import us.ihmc.javaFXToolkit.text.Text3D;
 
 public class MeshDataGeneratorVisualizer
 {
-   private static final boolean USE_TEXTURE = true;
+   private static final boolean USE_TEXTURE = false;
 
    public MeshDataGeneratorVisualizer(Stage primaryStage)
    {
       primaryStage.setTitle(getClass().getSimpleName());
 
       View3DFactory view3dFactory = new View3DFactory(600, 400);
-      view3dFactory.addCameraController(true);
-      view3dFactory.addWorldCoordinateSystem(0.4);
+      FocusBasedCameraMouseEventHandler cameraController = view3dFactory.addCameraController(true);
+      cameraController.changeCameraPosition(-1.0, -1.0, 1.0);
+      view3dFactory.addWorldCoordinateSystem(0.3);
+      view3dFactory.addNodeToView(createAxisLabels());
 
       PhongMaterial defaultMaterial;
       if (USE_TEXTURE)
@@ -44,113 +59,115 @@ public class MeshDataGeneratorVisualizer
          defaultMaterial = new PhongMaterial(Color.CYAN);
       }
 
-      MeshView arcTorus = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.ArcTorus(0.0, 1.0 * Math.PI, 0.15, 0.05, 64)));
+      MeshView arcTorus = new MeshView(interpretMeshData(MeshDataGenerator.ArcTorus(0.0, 1.0 * Math.PI, 0.15, 0.05, 64)));
       arcTorus.setMaterial(defaultMaterial);
       arcTorus.setTranslateX(0.0);
       arcTorus.setTranslateY(0.0);
       view3dFactory.addNodeToView(arcTorus);
 
-      MeshView cylinder = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.Cylinder(0.1, 0.3, 64, false)));
+      MeshView cylinder = new MeshView(interpretMeshData(MeshDataGenerator.Cylinder(0.1, 0.3, 64, true)));
       cylinder.setMaterial(defaultMaterial);
       cylinder.setTranslateX(0.0);
       cylinder.setTranslateY(0.5);
       view3dFactory.addNodeToView(cylinder);
 
-      MeshView flatRectangle = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.FlatRectangle(0.2, 0.3, 0.0)));
+      MeshView flatRectangle = new MeshView(interpretMeshData(MeshDataGenerator.FlatRectangle(0.2, 0.3, 0.0)));
       flatRectangle.setMaterial(defaultMaterial);
       flatRectangle.setTranslateX(0.0);
       flatRectangle.setTranslateY(1.0);
       view3dFactory.addNodeToView(flatRectangle);
 
-      MeshView cone = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.Cone(0.3, 0.1, 64)));
+      MeshView cone = new MeshView(interpretMeshData(MeshDataGenerator.Cone(0.3, 0.1, 64)));
       cone.setMaterial(defaultMaterial);
       cone.setTranslateX(0.0);
       cone.setTranslateY(-0.5);
       view3dFactory.addNodeToView(cone);
 
-      MeshView wedge = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.Wedge(0.3, 0.2, 0.2)));
+      MeshView wedge = new MeshView(interpretMeshData(MeshDataGenerator.Wedge(0.3, 0.2, 0.2)));
       wedge.setMaterial(defaultMaterial);
       wedge.setTranslateX(0.0);
       wedge.setTranslateY(-1.0);
       view3dFactory.addNodeToView(wedge);
 
-      MeshView sphere = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.Ellipsoid(0.15, 0.05, 0.2, 64, 64)));
+      MeshView sphere = new MeshView(interpretMeshData(MeshDataGenerator.Ellipsoid(0.15, 0.05, 0.2, 64, 64)));
       sphere.setMaterial(defaultMaterial);
       sphere.setTranslateX(0.5);
       sphere.setTranslateY(0.0);
       view3dFactory.addNodeToView(sphere);
 
       List<? extends Point2DReadOnly> polygonVertices = createPolygon();
-      MeshView extrudedPolygon = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.ExtrudedPolygon(polygonVertices, 0.1)));
+      MeshView extrudedPolygon = new MeshView(interpretMeshData(MeshDataGenerator.ExtrudedPolygon(polygonVertices, 0.1)));
       extrudedPolygon.setMaterial(defaultMaterial);
       extrudedPolygon.setTranslateX(0.5);
       extrudedPolygon.setTranslateY(0.5);
       view3dFactory.addNodeToView(extrudedPolygon);
 
-      MeshView line = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.Line(0.0, 0.0, 0.2, 0.0, 0.5, 0.0, 0.01)));
+      MeshView line = new MeshView(interpretMeshData(MeshDataGenerator.Line(0.0, 0.0, 0.2, 0.0, 0.5, 0.0, 0.01)));
       line.setMaterial(defaultMaterial);
       line.setTranslateX(0.5);
       line.setTranslateY(1.0);
       view3dFactory.addNodeToView(line);
 
-      MeshView pyramidCube = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.PyramidBox(0.1, 0.2, 0.1, 0.25)));
+      MeshView pyramidCube = new MeshView(interpretMeshData(MeshDataGenerator.PyramidBox(0.1, 0.2, 0.1, 0.25)));
       pyramidCube.setMaterial(defaultMaterial);
       pyramidCube.setTranslateX(0.5);
       pyramidCube.setTranslateY(-0.5);
       view3dFactory.addNodeToView(pyramidCube);
 
-      MeshView capsule = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.Capsule(0.3, 0.1, 0.05, 0.15, 16, 16)));
+      MeshView capsule = new MeshView(interpretMeshData(MeshDataGenerator.Capsule(0.2, 0.025, 0.05, 0.07, 16, 16)));
       capsule.setMaterial(defaultMaterial);
       capsule.setTranslateX(0.5);
       capsule.setTranslateY(-1.0);
       view3dFactory.addNodeToView(capsule);
 
-      MeshView genTruncatedCone = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.TruncatedCone(0.3, 0.2, 0.07, 0.05, 0.1, 64)));
+      MeshView genTruncatedCone = new MeshView(interpretMeshData(MeshDataGenerator.TruncatedCone(0.25, 0.2, 0.07, 0.05, 0.1, 64, false)));
       genTruncatedCone.setMaterial(defaultMaterial);
       genTruncatedCone.setTranslateX(-0.5);
       genTruncatedCone.setTranslateY(0.0);
       view3dFactory.addNodeToView(genTruncatedCone);
 
-      MeshView hemiEllipsoid = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.HemiEllipsoid(0.3, 0.1, 0.4, 16, 16)));
+      MeshView hemiEllipsoid = new MeshView(interpretMeshData(MeshDataGenerator.HemiEllipsoid(0.10, 0.20, 0.2, 64, 64)));
       hemiEllipsoid.setMaterial(defaultMaterial);
       hemiEllipsoid.setTranslateX(-0.5);
       hemiEllipsoid.setTranslateY(0.5);
       view3dFactory.addNodeToView(hemiEllipsoid);
 
-      MeshView tetrahedron = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.Tetrahedron(0.3)));
+      MeshView tetrahedron = new MeshView(interpretMeshData(MeshDataGenerator.Tetrahedron(0.3)));
       tetrahedron.setMaterial(defaultMaterial);
       tetrahedron.setTranslateX(-0.5);
       tetrahedron.setTranslateY(1.0);
       view3dFactory.addNodeToView(tetrahedron);
 
-      MeshView polygon = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.PolygonCounterClockwise(null, polygonVertices)));
+      MeshView polygon = new MeshView(interpretMeshData(MeshDataGenerator.PolygonCounterClockwise(null, polygonVertices)));
       polygon.setMaterial(defaultMaterial);
       polygon.setTranslateX(-0.5);
       polygon.setTranslateY(-0.5);
       view3dFactory.addNodeToView(polygon);
 
-      MeshView cube = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.Box(0.1, 0.2, 0.3, false)));
+      MeshView polytope = new MeshView(interpretMeshData(MeshDataGenerator.ConvexPolytope(EuclidPolytopeFactories.newIcosahedron(0.2))));
+      polytope.setMaterial(defaultMaterial);
+      polytope.setTranslateX(-0.5);
+      polytope.setTranslateY(-1.0);
+      view3dFactory.addNodeToView(polytope);
+
+      MeshView cube = new MeshView(interpretMeshData(MeshDataGenerator.Box(0.1, 0.2, 0.3, true)));
       cube.setMaterial(defaultMaterial);
       cube.setTranslateX(1.0);
       cube.setTranslateY(0.0);
       view3dFactory.addNodeToView(cube);
 
-      MeshView polygon3D = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.PolygonCounterClockwise(polygonVertices.stream()
-                                                                                                                                             .map(p2D ->
-                                                                                                                                             {
-                                                                                                                                                Point3D p3D = new Point3D(p2D);
-                                                                                                                                                p3D.setZ(0.1
-                                                                                                                                                      * (Math.random()
-                                                                                                                                                            - 0.5));
-                                                                                                                                                return p3D;
-                                                                                                                                             })
-                                                                                                                                             .collect(Collectors.toList()))));
+      MeshView polygon3D = new MeshView(interpretMeshData(MeshDataGenerator.PolygonCounterClockwise(polygonVertices.stream().map(p2D ->
+      {
+         Point3D p3D = new Point3D(p2D);
+         p3D.setZ(0.1 * (Math.random() - 0.5));
+         return p3D;
+      }).collect(Collectors.toList()))));
       polygon3D.setMaterial(defaultMaterial);
       polygon3D.setTranslateX(1.0);
       polygon3D.setTranslateY(0.5);
       view3dFactory.addNodeToView(polygon3D);
 
-      MeshView torus = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.Torus(0.15, 0.05, 64)));
+      MeshView torus = new MeshView(interpretMeshData(MeshDataGenerator.Torus(0.15, 0.05, 64)));
       torus.setMaterial(defaultMaterial);
       torus.setTranslateX(1.0);
       torus.setTranslateY(-0.5);
@@ -167,6 +184,36 @@ public class MeshDataGeneratorVisualizer
       List<Point2D> vertices = polygon.getPolygonVerticesView().stream().map(Point2D::new).collect(Collectors.toList());
       Collections.reverse(vertices);
       return vertices;
+   }
+
+   private static Node createAxisLabels()
+   {
+      AxisAngle xAxisAngle = new AxisAngle();
+      xAxisAngle.appendYawRotation(-0.25 * Math.PI);
+      xAxisAngle.appendRollRotation(-0.5 * Math.PI);
+      Node xLabel = createLabel("x", Color.RED, 0.10, 0.001, new Point3D(300, 0, 25), xAxisAngle);
+
+      AxisAngle yAxisAngle = new AxisAngle();
+      yAxisAngle.appendYawRotation(-0.25 * Math.PI);
+      yAxisAngle.appendRollRotation(-0.5 * Math.PI);
+      Node yLabel = createLabel("y", Color.GREEN, 0.10, 0.001, new Point3D(0, 300, 25), yAxisAngle);
+
+      AxisAngle zAxisAngle = new AxisAngle();
+      zAxisAngle.appendYawRotation(-0.25 * Math.PI);
+      zAxisAngle.appendRollRotation(-0.5 * Math.PI);
+      Node zLabel = createLabel("z", Color.BLUE, 0.10, 0.001, new Point3D(0, 0, 325), zAxisAngle);
+      return new Group(xLabel, yLabel, zLabel);
+   }
+
+   private static Node createLabel(String text, Color color, double height, double thickness, Point3DReadOnly position, Orientation3DReadOnly orientation)
+   {
+      Text3D label = new Text3D(text);
+      label.setFontHeight(height);
+      label.setFontThickness(thickness);
+      ((Text3DMesh) label.getNode()).getChildren().forEach(child -> ((TexturedMesh) child).setMaterial(new PhongMaterial(color)));
+      label.getNode().getTransforms().add(JavaFXTools.createAffineFromOrientation3DAndTuple(orientation, position));
+
+      return label.getNode();
    }
 
    public static void main(String[] args)
