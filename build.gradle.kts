9387b9484c1c3971eb27db73bfb63e6e983bce73
Kevin Harrington <kevin.harrington@halodi.com>
2021-07-23 14:14:11 -0400

    put the deps back the way they were int he upstream

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,9 +25,11 @@ ihmc {
 mainDependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
    api("org.apache.commons:commons-lang3:3.11")
- 
-   api("org.fxyz3d:fxyz3d:0.5.4")
 
+   api("org.fxyz3d:fxyz3d:0.1.1") {
+      exclude(group = "java3d", module = "vecmath")
+      exclude(group = "org.slf4j", module = "slf4j-simple")
+   }
 
    api("us.ihmc:euclid:0.16.0")
    api("us.ihmc:ihmc-yovariables:0.9.8")
