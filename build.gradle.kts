16e76d38105bcf13cb26b630fe556df23737bacf
Kevin Harrington <kevin.harrington@halodi.com>
2021-07-23 12:56:25 -0400

    Update to latest version to avoid erronious dep of JCSG

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,10 +26,7 @@ mainDependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
    api("org.apache.commons:commons-lang3:3.11")
  
-   api("org.fxyz3d:fxyz3d:0.1.1"){		￼
-￼      exclude(group = "java3d", module = "vecmath")		
-￼      exclude(group = "org.slf4j", module = "slf4j-simple")		
-￼   }
+   api("org.fxyz3d:fxyz3d:0.5.4")
 
 
    api("us.ihmc:euclid:0.16.0")
