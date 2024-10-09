af95cfc10232cb0d5129642818324d9f6552065a
Dexton Anderson <danderson@ihmc.org>
2024-10-09 09:54:23 -0500

    :bookmark: 17-0.22.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "17-0.22.9"
+   version = "17-0.22.10"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-javafx-toolkit"
    openSource = true
    
