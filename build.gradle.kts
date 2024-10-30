0c555c6def4880c2fea72155f1097ead9c10d101
rgriffin@ihmc.org <rgriffin@ihmc.us>
2024-10-30 17:16:53 -0500

    :bookmark: 17-0.22.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "17-0.22.10"
+   version = "17-0.22.11"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-javafx-toolkit"
    openSource = true
    
