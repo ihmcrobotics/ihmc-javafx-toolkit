36e45da326f19eebff9d0e2e50bb181f28c4e290
rgriffin@ihmc.org <rgriffin@ihmc.us>
2024-10-30 17:16:30 -0500

    :arrow_up: euclid 0.22.2 :arrow_up: ihmc-graphics-description 0.25.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ mainDependencies {
       exclude(group = "org.slf4j", module = "slf4j-simple")
    }
 
-   api("us.ihmc:euclid:0.21.0")
-   api("us.ihmc:ihmc-graphics-description:0.25.1")
+   api("us.ihmc:euclid:0.22.2")
+   api("us.ihmc:ihmc-graphics-description:0.25.2")
    api("us.ihmc:jassimp:4.0.0-ihmc6")
 
    var javaFXVersion = "17.0.2"
