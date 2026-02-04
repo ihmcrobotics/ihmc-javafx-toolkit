78a61d498621ff9514f82f0f5ba1f1d14a837614
Robert <rjgriffin42@gmail.com>
2026-02-04 16:52:24 -0600

    :arrow_up: ihmc-graphics-description 0.27.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ mainDependencies {
    }
 
    api("us.ihmc:euclid:0.22.5")
-   api("us.ihmc:ihmc-graphics-description:0.26.2")
+   api("us.ihmc:ihmc-graphics-description:0.27.0")
    api("us.ihmc:jassimp:4.0.0-ihmc6")
 
    var javaFXVersion = "17.0.8"
